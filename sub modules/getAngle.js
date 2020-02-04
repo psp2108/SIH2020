210cb2592cc6c80e39818c2b0e5dd76c2bfb8b14
Pratik Panchal <pratik.sp.1112@gmail.com>
2020-02-04 13:00:33 +0530

    find angle function

---
+++ sub modules/getAngle.js
@@ -0,0 +1,15 @@
+function getAngle(pts, angle){
+    /*
+        {
+            p1: {
+                x: 1,
+                y: 2
+            },
+            p2: {
+                x: 3,
+                y: 3
+            }
+
+        }
+    */
+}
