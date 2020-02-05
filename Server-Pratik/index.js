5e4458bcf12715eb272e4905c451c0e6abb6f6da
Pratik Panchal <pratik.sp.1112@gmail.com>
2020-02-05 06:58:24 +0530

    res printed test

--- Server-Pratik/index.js
+++ Server-Pratik/index.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
 
 var port = process.env['SIH_PORT'] || 3000;
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello world')
+    console.log(req);
+    res.send('hello world');
 })
 
-app.listen(port)
+app.listen(port);
