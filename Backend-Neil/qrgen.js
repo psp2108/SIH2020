ad95542255dcfc6810fc18fc363f1916b5e1a6e2
Pratik Panchal <pratik.sp.1112@gmail.com>
2020-02-06 20:34:54 +0530

    QR changed

--- Backend-Neil/qrgen.js
+++ Backend-Neil/qrgen.js
@@ -24,7 +24,8 @@ console.log(dumData);
 console.log(QRCode);
 // QRCode.create("Neil");
 // QRCode.toFile('QRs/filename.png', [{ data: 'ABCDEFG', mode: 'alphanumeric'}], {
-QRCode.toFile('QRs/filename-realishdata.png', [{ data: dumData, mode: 'byte'}], {
+// QRCode.toFile('QRs/filename-realishdata.png', [{ data: dumData, mode: 'byte'}], {
+QRCode.toFile('QRs/filename-realishdata.png', [{ data: [5, 10, 0, 255, 45, 68], mode: 'byte'}], {
     color: {
       dark: '#000000',  // Blue dots
       light: '#ffffff' // Transparent background
