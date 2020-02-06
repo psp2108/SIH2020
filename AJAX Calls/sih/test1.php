61ac9b17951dc6467ab6a1bc29143fa4adc8ef6c
tanmaiprabhune <tanmai.prabhune@gmail.com>
2020-02-06 10:09:59 +0530

    test json

--- AJAX Calls/sih/test1.php
+++ AJAX Calls/sih/test1.php
@@ -42,7 +42,104 @@ function sendQR(QRid)
                 //alert(JSON.stringify(parsed));
                 var floor_no=0
                 alert(JSON.stringify(parsed["meta-data"]["maps"]["floor"+floor_no]));
+                
+                function send_Json_for_directions{
+                    var map = {
+                        "0": {"1":225},
+                        "1": {"0":45, "1":0, "2":270},
+                        "2": {"2":90, "3":0},
+                        "3": {"2":180, "4":0},
+                        "4": {"3":180, "5":90},
+                        "5": {"4":270, "5":180, "6":90},
+                        "6": {"5":270},
+                        "file-path" : "location of .dwg or any map file"
+                    }
 
+                    var routeTable = [
+                        { "t": 0, "p": 0 },
+                        { "t": 1, "p": 0 },
+                        { "t": 2, "p": 1 },
+                        { "t": 3, "p": 2 },
+                        { "t": 4, "p": 3 },
+                        { "t": 5, "p": 4 },
+                        { "t": 6, "p": 5 }
+                    ]
+
+                    var target = 6;
+                    var source = 1;
+                    get_direction(map, routeTable, target, source);
+                }
+
+                fuction get_direction(map, routeTable, target, source){
+                    var getDirections = function(map, routeTable, target, source){
+                    var faceAngel = map[source][source];
+                    var path = [];
+                    var directions = [];
+                    var notation = {
+                    "n": "Go Straight",
+                    "ne": "Slight Right",
+                    "e": "Turn Right",
+                    "se": "Turn Around and Slight Left",
+                    "s": "Turn Around",
+                            "sw": "Turn Around and Slight Right",
+                            "w": "Turn Left",
+                            "nw": "Slight Left"
+                        };
+                        
+                        var getPath = function(t,s){
+                            if (t != s){
+                                getPath(routeTable[t].p, s);
+                                // path.push(t);
+                            }
+                            path.push(t);
+                        }
+
+                        getPath(target, source);
+
+                        for(var i=1; i<path.length; i++){
+                            var temp = map[i-1][i] - faceAngel;
+                            temp = temp < 0 ? 360 + temp : temp;
+                            faceAngel = map[i-1][i];
+
+                            if((temp >= 338 && temp < 360) || (temp >= 0 && temp < 23)){
+                                directions.push(notation.n)
+                            }
+                            else if(temp >= 23 && temp < 68){
+                                directions.push(notation.ne)
+                            }
+                            else if(temp >= 68 && temp < 113){
+                                directions.push(notation.e)
+                            }
+                            else if(temp >= 113 && temp <= 158){
+                                directions.push(notation.se)
+                            }
+                            else if(temp >= 158 && temp < 203){
+                                directions.push(notation.s)
+                            }
+                            else if(temp >= 203 && temp < 248){
+                                directions.push(notation.sw)
+                            }
+                            else if(temp >= 248 && temp < 293){
+                                directions.push(notation.w)
+                            }
+                            else if(temp >= 293 && temp < 338){
+                                directions.push(notation.nw)
+                            }
+                            else{
+                                console.log("Angle above 360");
+                            }
+                        }
+
+                        return {path: path, directions: directions};
+                    }
+
+
+                    
+
+                    var t = getDirections(map, routeTable, target, source)
+                    //console.log(t)
+                    alert(t);
+                }
 
                 
             },
@@ -53,7 +150,11 @@ function sendQR(QRid)
         }
     );
 
-    
+    // $.get("http://localhost:3000/getBuilding/:123", {number: 345} , function(data){
+    //     // Display the returned data in browser
+    //     console.log(data);
+    //     alert(JSON.stringify(data));
+    // });
     
 }
 
