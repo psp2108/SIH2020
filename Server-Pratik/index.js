d21b5e9c4c724f65e3b48b0da72262587be11f2d
Pratik Panchal <pratik.sp.1112@gmail.com>
2020-02-07 08:00:16 +0530

    code refactor

--- Server-Pratik/index.js
+++ Server-Pratik/index.js
@@ -155,180 +155,22 @@ app.get('/getPdf/:id', function(req, res){
     res.download(file); // Set disposition and send it.
 });
 
-app.get('/getBuilding/:id', function(req, res){  
-    data = {
-        "meta-data" : {
-            "name" : "SAMPLE BUILDING NAME",
-            "registration-id" : 345234,
-            "total-floors" : 2,
-    
-            "maps" : {
-                "floor0" : {
-                    "0": {"1":225},
-                    "1": {"0":45, "1":0, "2":270},
-                    "2": {"2":90, "3":0},
-                    "3": {"2":180, "4":0},
-                    "4": {"3":180, "5":90},
-                    "5": {"4":270, "5":180, "6":90},
-                    "6": {"5":270},
-                    "file-path" : "location of .dwg or any map file"
-                },
-                "floor1" : {
-                    "0": {"1":225},
-                    "1": {"0":45, "1":0, "2":270},
-                    "2": {"2":90, "3":0},
-                    "3": {"2":180, "4":0},
-                    "4": {"3":180, "5":90},
-                    "5": {"4":270, "5":180, "6":90},
-                    "6": {"5":270},
-                    "file-path" : "location of .dwg or any map file"
-                } 
-            }
-        },
-    
-        "floor-destination" : {
-            "floor0" : {
-                "name" : "",
-                "destinations" : [
-                    {
-                        "name" : "Reception",
-                        "node" : 0,
-                        "type" : "destination"
-                    },
-                    {
-                        "name" : "Lift",
-                        "node" : 2,
-                        "type" : "via",
-                        "direction" : "both"
-                    },
-                    {
-                        "name" : "Stairs",
-                        "node" : 1,
-                        "type" : "via",
-                        "direction" : "both"
-                    },
-                    {
-                        "name" : "Lunch",
-                        "node" : 3,
-                        "type" : "destination"
-                    },
-                    {
-                        "name" : "Escalator",
-                        "node" : 6,
-                        "type" : "via",
-                        "direction" : "up"
-                    },
-                    {
-                        "name" : "Escalator",
-                        "node" : 5,
-                        "type" : "via",
-                        "direction" : "down"
-                    }
-                ]
-            },
-            "floor1" : {
-                "name" : "",
-                "destinations" : [
-                    {
-                        "name" : "Rest Room",
-                        "node" : 0,
-                        "type" : "destination"
-                    },
-                    {
-                        "name" : "Lift",
-                        "node" : 2,
-                        "type" : "via",
-                        "direction" : "both"
-                    },
-                    {
-                        "name" : "Stairs",
-                        "node" : 1,
-                        "type" : "via",
-                        "direction" : "both"
-                    },
-                    {
-                        "name" : "Terrace",
-                        "node" : 3,
-                        "type" : "destination"
-                    },
-                    {
-                        "name" : "Escalator",
-                        "node" : 6,
-                        "type" : "via",
-                        "direction" : "up"
-                    },
-                    {
-                        "name" : "Escalator",
-                        "node" : 5,
-                        "type" : "via",
-                        "direction" : "down"
-                    }
-                ]
-            }
-        },
-        
-        "source-node": [
-            {
-                "node" : 1,
-                "floor" : 0,
-                "qr-id" : "[building reg no]-[floor number 3 digit]-[0 to 'n']"
-            },
-            {
-                "node" : 4,
-                "floor" : 1,
-                "qr-id" : "[building reg no]-[floor number 3 digit]-[0 to 'n']"
-            },
-            {
-                "node" : 1,
-                "floor" : 0,
-                "qr-id" : "",
-                "path-table" : [
-                    { "t": 0, "p": 0},
-                    { "t": 1, "p": 0},
-                    { "t": 2, "p": 3 },
-                    { "t": 3, "p": 4 },
-                    { "t": 4, "p": 5 },
-                    { "t": 5, "p": 6 },
-                    { "t": 6, "p": 9 }
-                ]
-            }
-        ]
-    };
-    console.log(req, "Requested")
-    res.setHeader('content-type', 'application/json');
-    res.status(200).json(data);
-});
-
-app.get('/g/:i', function(req, resp){
-    console.log(req.params.i);
-
+app.get('/getBuilding/:id', function(req, resp){
     MongoClient.connect(mongoURL, function(err, db) {
         if (err){
             resp.status(200).json({message : "Error: DB not connecting", status : "failed"})
         }
         var dbo = db.db(mongoDB);
         // dbo.collection("buildings").find({"meta-data" : {"registration-id" : "00022"}}).toArray( function(err, res) {
-        dbo.collection("buildings").find({'_id' : req.params.i}).toArray( function(err, res) {
+        dbo.collection("buildings").find({'_id' : req.params.id}).toArray( function(err, res) {
           if (err) {
             resp.status(200).json({message : "Error: DB not connecting", status : "failed"});
           };
           db.close();
-          console.log("---------------")
-          console.log(res)
-          console.log("---------------")
-          resp.status(200).send(res);
+          resp.status(200).send(res[0]);
         });
     });
-})
-
-// app.post('/regBuilding',function(req, res){
-
-
-// });
-
-// app.post('/setMap',function(req, res){
-
-// });
+});
 
 app.use(router);
 app.listen(port);
